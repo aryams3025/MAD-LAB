0392315d4afd649303fad0e024b89b987f281ee1
aryams3025 <80573229+aryams3025@users.noreply.github.com>
2022-08-31 11:33:02 +0530

    Create loginform.java

---
+++ loginform.java
@@ -0,0 +1,37 @@
+package com.example.arya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    EditText usr,pswd;
+    Button submit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        usr = (EditText)findViewById(R.id.usr);
+        pswd = (EditText)findViewById(R.id.pswd);
+        submit = (Button)findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(usr.getText().toString().equals("admin") &&  pswd.getText().toString().equals("admin")){
+                    Toast.makeText(getApplicationContext(),"Redirecting",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Invalid Credentials",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
+
+
